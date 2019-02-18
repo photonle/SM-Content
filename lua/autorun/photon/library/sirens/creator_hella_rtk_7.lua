eba025cfbe6f287ea54d9426b2f66724b5c51cc7
creator_2013 <creator.email.yt@gmail.com>
2019-02-18 17:47:32 +0100

    Added more Hella RTK 7 sirens

--- lua/autorun/photon/library/sirens/creator_hella_rtk_7.lua
+++ lua/autorun/photon/library/sirens/creator_hella_rtk_7.lua
@@ -1,7 +1,8 @@
 if not EMVU then return end
 
-local siren = {
-	Name = "Deutsch",
+-- German Siren
+EMVU.AddCustomSiren("creator_hella_rtk_7_de", {
+	Name = "Deutsche (DE)",
 	Category = "Hella RTK 7",
 	Set = {
 		{
@@ -21,5 +22,122 @@ local siren = {
 		}
 	},
 	Horn = "emv/sirens/Hella RTK 7/horn.wav"
-}
-EMVU.AddCustomSiren("creator_hella_rtk_7_de", siren)
+})
+
+-- US Siren
+EMVU.AddCustomSiren("creator_hella_rtk_7_us", {
+	Name = "USA",
+	Category = "Hella RTK 7",
+	Set = {
+		{
+			Name = "WAIL",
+			Sound = "emv/sirens/Hella RTK 7/us_wail.wav",
+			Icon = "wail"
+		},
+		{
+			Name = "YELP",
+			Sound = "emv/sirens/Hella RTK 7/us_yelp.wav",
+			Icon = "yelp"
+		},
+		{
+			Name = "HILO",
+			Sound = "emv/sirens/Hella RTK 7/us_hilo.wav",
+			Icon = "hilo"
+		}
+	},
+	Horn = "emv/sirens/Hella RTK 7/horn.wav"
+})
+
+-- French Siren
+EMVU.AddCustomSiren("creator_hella_rtk_7_fr_police", {
+	Name = "Police Nationale (FR)",
+	Category = "Hella RTK 7",
+	Set = {
+		{
+			Name = "HILO",
+			Sound = "emv/sirens/Hella RTK 7/fr_police.wav",
+			Icon = "hilo"
+		},
+	},
+	Horn = "emv/sirens/Hella RTK 7/horn.wav"
+})
+
+EMVU.AddCustomSiren("creator_hella_rtk_7_fr_gendarmerie", {
+	Name = "Gendarmerie (FR)",
+	Category = "Hella RTK 7",
+	Set = {
+		{
+			Name = "HILO",
+			Sound = "emv/sirens/Hella RTK 7/fr_gendarmerie.wav",
+			Icon = "hilo"
+		},
+	},
+	Horn = "emv/sirens/Hella RTK 7/horn.wav"
+})
+
+EMVU.AddCustomSiren("creator_hella_rtk_7_fr_firefighter", {
+	Name = "Pompiers (FR)",
+	Category = "Hella RTK 7",
+	Set = {
+		{
+			Name = "HILO",
+			Sound = "emv/sirens/Hella RTK 7/fr_firefighter.wav",
+			Icon = "hilo"
+		},
+	},
+	Horn = "emv/sirens/Hella RTK 7/horn.wav"
+})
+
+EMVU.AddCustomSiren("creator_hella_rtk_7_fr_ambulance", {
+	Name = "Ambulance (FR)",
+	Category = "Hella RTK 7",
+	Set = {
+		{
+			Name = "HILO",
+			Sound = "emv/sirens/Hella RTK 7/fr_ambulance.wav",
+			Icon = "hilo"
+		},
+	},
+	Horn = "emv/sirens/Hella RTK 7/horn.wav"
+})
+
+-- Italian Siren
+EMVU.AddCustomSiren("creator_hella_rtk_7_it_police", {
+	Name = "Polizia (IT)",
+	Category = "Hella RTK 7",
+	Set = {
+		{
+			Name = "HILO",
+			Sound = "emv/sirens/Hella RTK 7/it_police.wav",
+			Icon = "hilo"
+		},
+	},
+	Horn = "emv/sirens/Hella RTK 7/horn.wav"
+})
+
+EMVU.AddCustomSiren("creator_hella_rtk_7_it_firefighter", {
+	Name = "Firefighter (IT)",
+	Category = "Hella RTK 7",
+	Set = {
+		{
+			Name = "HILO",
+			Sound = "emv/sirens/Hella RTK 7/it_firefighter.wav",
+			Icon = "hilo"
+		},
+	},
+	Horn = "emv/sirens/Hella RTK 7/horn.wav"
+})
+
+-- Dutch Siren
+EMVU.AddCustomSiren("creator_hella_rtk_7_nl_twotone", {
+	Name = "TwoTone (NL)",
+	Category = "Hella RTK 7",
+	Set = {
+		{
+			Name = "HILO",
+			Sound = "emv/sirens/Hella RTK 7/nl_twosound.wav",
+			Icon = "hilo"
+		},
+	},
+	Horn = "emv/sirens/Hella RTK 7/horn.wav"
+})
